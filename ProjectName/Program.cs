51a88cb4f430c816ae1d57c49cff93120e28c7d2
harunozturk <harun.ozturk@bil.omu.edu.tr>
2014-10-18 17:29:56 +0300

    kullanıcı kodu eklendi

--- ProjectName/Program.cs
+++ ProjectName/Program.cs
@@ -6,7 +6,29 @@ namespace ProjectName
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+			int tekrar = 1;
+			if (args.Length == 2) 
+			{
+				if (args [0] == "en" & int.TryParse (args [1], out tekrar))
+					Console.WriteLine ("ingilizce " + tekrar + " tane");
+				else
+					Console.WriteLine ("help();");
+			} 
+			else if (args.Length == 1) 
+			{
+				if (args [0] == "en")
+					Console.WriteLine ("ingilizce " + tekrar + " tane");
+				else if (int.TryParse (args [0], out tekrar))
+					Console.WriteLine ("turkce " + tekrar + " tane");
+				else
+					Console.WriteLine ("help();");
+			} 
+			else if (args.Length == 0)
+			{
+				Console.WriteLine ("turkce " + tekrar + " tane");
+			}
+			else
+				Console.WriteLine ("help();");
 		}
 	}
 }
