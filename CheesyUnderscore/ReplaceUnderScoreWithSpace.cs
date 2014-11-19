e8de32602e9464d3c966930f118e0af8da94792a
handankara <handan.kara@bil.omu.edu.tr>
2014-11-19 20:51:08 +0200

    alttire silen kod

---
+++ CheesyUnderscore/ReplaceUnderScoreWithSpace.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+
+namespace ReplaceUnderScoreWithSpace
+{
+    static class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            foreach (string arg in args)
+            {
+                arg.ReplaceUnderScoreWithSpace(); //extension method
+			}
+			Console.WriteLine(chars);
+        }
+
+        public static void ReplaceUnderScoreWithSpace (this string arg)
+        {
+				char[] chars = arg.ToCharArray();
+				int i = 0;
+				
+				while (i < chars.Length)
+				{
+					if (chars[i].ToString() != "_")
+						break;
+					i++;
+				}
+				int j = (chars.Length)-1;
+				while ( j >= 0)
+				{
+					if (chars[j].ToString() != "_")
+						break;
+					j--;
+				}
+				for (int c = i; c < j; c++)
+				{
+					chars[c] = (chars[c] == '_') ? ' ' : chars[c];
+				}
+				
+        }
+    }
+}
