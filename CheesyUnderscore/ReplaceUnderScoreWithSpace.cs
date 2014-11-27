ec9c5ec0df11d11b44f732c03c4007d704c422e0
harunozturk <harun.ozturk@bil.omu.edu.tr>
2014-11-27 20:14:23 +0200

    stil duzenlendi

--- CheesyUnderscore/ReplaceUnderScoreWithSpace.cs
+++ CheesyUnderscore/ReplaceUnderScoreWithSpace.cs
@@ -2,42 +2,38 @@ using System;
 using System.Collections.Generic;
 
 namespace ReplaceUnderScoreWithSpace
-{   
-    static class MainClass
-    {
-    	private static char[] chars;
-    	
-        public static void Main (string[] args)
-        {
-        	foreach (string arg in args)
-            	{
-                	arg.ReplaceUnderScoreWithSpace(); //extension method
-	    	}
-	        Console.WriteLine(chars);
-        }
+{
+	static class MainClass
+	{
+		private static char[] chars;
 
-        public static void ReplaceUnderScoreWithSpace (this string arg)
-        {
-		chars = arg.ToCharArray();
-		int i = 0;
-		int j = (chars.Length) - 1;
+		public static void Main (string[] args)
+		{
+			foreach (string arg in args) {
+				arg.ReplaceUnderScoreWithSpace (); //extension method
+			}
+			Console.WriteLine (chars);
+		}
+
+		public static void ReplaceUnderScoreWithSpace (this string arg)
+		{
+			chars = arg.ToCharArray ();
+			int i = 0;
+			int j = (chars.Length) - 1;
 		
-		while (i < chars.Length)
-		{
-			if (chars[i].ToString() != "_")
-				break;
-			i++;
-		}
-		while (j >= 0)
-		{
-			if (chars[j].ToString() != "_")
-				break;
-			j--;
-		}
-		for (int c = i; c < j; c++)
-		{
-			chars[c] = (chars[c] == '_') ? ' ' : chars[c];
+			while (i < chars.Length) {
+				if (chars [i].ToString () != "_")
+					break;
+				i++;
+			}
+			while (j >= 0) {
+				if (chars [j].ToString () != "_")
+					break;
+				j--;
+			}
+			for (int c = i; c < j; c++) {
+				chars [c] = (chars [c] == '_') ? ' ' : chars [c];
+			}
 		}
 	}
-    }
 }
--- CheesyUnderscore/test.cs
+++ CheesyUnderscore/test.cs
@@ -1,8 +1,12 @@
 [TestMethod()]
 public void ReplaceUnderScoreWithSpaceTest(){
 	string testparam1 = "foobar";
+	string testparam2 = "foo_bar;
 	CheesyUnderscore cheesy = new CheesyUnderscore();
-	string expected = "foobar";
-	string actual = cheesy.ReplaceUnderScoreWithSpace(testparam1);
-	Assert.AreEqual(expected, actual);
+	string expected1 = "foobar";
+	string expected2 = "foo bar";
+	string actual1 = cheesy.ReplaceUnderScoreWithSpace(testparam1);
+	string actual2 = cheesy.ReplaceUnderScoreWithSpace(testparam2);
+	Assert.AreEqual(expected1, actual1);
+	Assert.AreEqual(expected2, actual2);
 }
